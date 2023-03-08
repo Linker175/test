66fc3db8cc182fa7eb2deaeddd0bb2f27b89c341
Yoann <thebaultyoann@gmail.com>
2023-03-08 11:43:37 +0100

    ok

--- Hello.py
+++ Hello.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask 
 app = Flask(__name__)
 
 @app.route('/')
@@ -6,5 +6,5 @@ def hello_world():
    return 'Hello World'
 
 if __name__ == '__main__':
-   app.run("0.0.0.0", 5000, False)
+   app.run("park.sandbox.ensea.fr", 443, False)
 
