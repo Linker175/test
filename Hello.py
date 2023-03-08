dafaa459dc286fdec35563de4d6129b94895f58f
Yoann <thebaultyoann@gmail.com>
2023-03-08 16:30:06 +0100

    ok

--- Hello.py
+++ Hello.py
@@ -1,17 +1,18 @@
-from flask import Flask 
-
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def get_request():
-    # Gérer la requête GET ici
-    return 'Hello ESP32!'
-
-@app.route('/')
 def index():
-    return 'Hello World!'
+    return "Hellomyfriend"
+
+@app.route('/code', methods=['GET'])
+def get_code():
+    # récupérer le code ici et le stocker dans une variable 'code'
+    code = 33
+    return code
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=80)
+    app.run(debug=True, host='0.0.0.0', port=80)
+
         
 
