962d654a28c311242bd246d5d42f1887367f7f8b
Yoann <thebaultyoann@gmail.com>
2023-03-08 16:17:55 +0100

    ok

--- Hello.py
+++ Hello.py
@@ -1,10 +1,17 @@
 from flask import Flask 
+
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])
+def get_request():
+    # Gérer la requête GET ici
+    return 'Hello ESP32!'
+
 @app.route('/')
-def hello_world():
-   return 'Hello World'
+def index():
+    return 'Hello World!'
 
 if __name__ == '__main__':
-   app.run("0.0.0.0", 80, False)
+    app.run(host='0.0.0.0', port=80)
+        
 
